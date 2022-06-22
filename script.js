57a5cc48a74c834ee4fa62fbfe16bf56cecbb34e
TedHamilton <tedwarehamilton@gmail.com>
2022-06-21 22:37:25 -0400

    trying to code for random password in js

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 // Assignment code here
 window.alert ("Click button for password!!!");
-var number
-function generatePassword() {
-  var number length = 9,
-  charset="abcdefghijklmnopqrstuvwrxyzABCDEFGHIJKLMNOPQRSTUVWRXYZ1234567890",
-  retVal = "";
-  for (var i=0), n=charset.length; i<length; ++i {
-    retVal +=charset.charAt(Math.floor(Math.random()*n));
-  }
-  return retVal;
-}
-
+ 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
+ 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  var number;
+  var generateBtnEl = $("#generateBtn");
+  function generatePassword() {
+    var number length = 9,
+    charset="abcdefghijklmnopqrstuvwrxyzABCDEFGHIJKLMNOPQRSTUVWRXYZ1234567890",
+    retVal = "";
+    for (var i=0), n=charset.length; i<length; ++i {
+      retVal +=charset.charAt(Math.floor(Math.random()*n));
+    }
+    return retVal;
+  }
+ 
   passwordText.value = password;
-
+ 
 }
-
+ 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 addEventListenerByClass("btn")
