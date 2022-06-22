04421a8837ccec3705900cca4de7f52a8f65a5ca
TedHamilton <tedwarehamilton@gmail.com>
2022-06-22 00:17:09 -0400

    fixing js

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
 // Assignment code here
 window.alert ("Click button for password!!!");
+
+
  
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
- 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = document.getElementById("password")
   var passwordText = document.querySelector("#password");
-  var number;
-  var generateBtnEl = $("#generateBtn");
-  function generatePassword() {
-    var number length = 9,
-    charset="abcdefghijklmnopqrstuvwrxyzABCDEFGHIJKLMNOPQRSTUVWRXYZ1234567890",
-    retVal = "";
-    for (var i=0), n=charset.length; i<length; ++i {
-      retVal +=charset.charAt(Math.floor(Math.random()*n));
-    }
-    return retVal;
-  }
- 
+  var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var passwordLength = 9;
+  var password = "";
   passwordText.value = password;
  
 }
  
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-addEventListenerByClass("btn")
