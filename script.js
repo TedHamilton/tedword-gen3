163b75fcafd6f78b551671c2ba658ff6253e014c
TedHamilton <tedwarehamilton@gmail.com>
2022-06-25 15:19:15 -0400

    took out uneeded code, and added ;'s

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ window.alert ("Click button for password!!!");
 var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
-  var password = document.getElementById("password")
+  var password = document.getElementById("password");
   var passwordText = document.querySelector("#password");
   var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var passwordLength = 9;
