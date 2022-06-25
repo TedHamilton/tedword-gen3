24b49b4a182c2ff2e799c3ef01d66ca4c169fc70
TedHamilton <tedwarehamilton@gmail.com>
2022-06-25 13:53:31 -0400

    added vars and func. Made the gen button.

--- script.js
+++ script.js
@@ -12,7 +12,12 @@ function writePassword() {
   var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var passwordLength = 9;
   var password = "";
-  passwordText.value = password;
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+  }
+  
+  document.getElementById("password").value = password;
  
 }
  
