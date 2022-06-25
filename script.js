8c37f64b0cb97cbae40a494feee8e7b71324a4fc
TedHamilton <tedwarehamilton@gmail.com>
2022-06-25 17:20:10 -0400

    adding functions for uppercase and #'s

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // Assignment code here
 window.alert ("Click button for password!!!");
 
-function getRandomLower() {
- return String.fromCharCode(97);   
-}
  
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -11,9 +8,25 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = document.getElementById("password");
   var passwordText = document.querySelector("#password");
-  var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var passwordLength = 9;
-  var password = "";
+
+
+  function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+   
+   }
+   console.log(getRandomLower)
+
+   function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+   
+   }
+   console.log(getRandomUpper)
+
+   function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+   
+   }
+   console.log(getRandomLowercase)
   
   
   document.getElementById("password").value = password;
