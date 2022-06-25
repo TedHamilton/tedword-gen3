4ed65c5e92bf0bb1515eb4f73db7b7d8a1e47523
TedHamilton <tedwarehamilton@gmail.com>
2022-06-25 17:33:33 -0400

    adding function for symbols

--- script.js
+++ script.js
@@ -14,19 +14,22 @@ function writePassword() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
    
    }
-   console.log(getRandomLower)
 
    function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
    
    }
-   console.log(getRandomUpper)
 
    function getRandomNumber() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
    
    }
-   console.log(getRandomLowercase)
+
+   function getRandomSymbol() {
+    const symbols="!@#$%^&*(),.";
+    return symbols[Math.floor(Math.random() * Symbol.length)];
+   
+   }
   
   
   document.getElementById("password").value = password;
