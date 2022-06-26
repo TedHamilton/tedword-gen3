1440c0ac9b182b28e2be129865d226245eecf0de
TedHamilton <tedwarehamilton@gmail.com>
2022-06-25 23:48:16 -0400

    added generating functions

--- script.js
+++ script.js
@@ -9,12 +9,46 @@ function writePassword() {
   var password = document.getElementById("password");
   var passwordText = document.querySelector("#password");
 
+const resultEl = document.getElementById("result")
+const lengthEl = document.getElementById("length")
+const uppercaseaEl = document.getElementById("uppercase")
+const lowercaseEl = document.getElementById("lowercase")
+const numbersEl = document.getElementById("numbers")
+const symbolsEl = document.getElementById("symbols")
+const generateEl = document.getElementById("generate")
+const clipboardEl = document.getElementById("clipboard")
+
 const randomFunc = {
   lower: getRandomLower,
   upper: getRandomUpper,
   number: getRandomNumber,
   symbol: getRandomSymbol
 };
+
+generateEl.addEventListener("click" , () =>{
+  const length = lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = lowercaseEl.checked;
+  const hasSymbol = lowercaseEl.checked;
+
+  resultEl.innertext = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+
+});
+
+
+function generatePassword(lower, upper, number, symbol, length){
+  
+}
+
+
+//generating functions
   function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
    
